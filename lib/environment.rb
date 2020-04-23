5b7f42d9f9de0189e7056ccd398e1406ee18ca88
Christopher Dent <christopherdent01@gmail.com>
2020-04-22 22:49:43 -0400

    Fixed error with pry and updated readme

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,14 +1,14 @@
-#environment file 
+#environment file
 require_relative "./prehistoric_life/version"
 require_relative "prehistoric_life/cli"
 require_relative "prehistoric_life/period"
 require_relative "prehistoric_life/scraper"
 require_relative "prehistoric_life/creature"
 
-require 'colorize' 
+require 'colorize'
 require 'open-uri'
 require 'nokogiri'
-require 'pry'
+ 
 
 module PrehistoricLife
   class Error < StandardError; end
